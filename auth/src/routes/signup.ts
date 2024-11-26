1455f7d7248a5ecf5d6a3d10d6644edb03ee939f
H1ep Vu <hiepvu.tech02@gmail.com>
2024-11-26 22:05:19 +0700

    generating a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
-import { body, validationResult } from 'express-validator'
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-errors';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -33,6 +34,15 @@ router.post('/api/users/signup', [
 
         const user = User.build({ email, password });
         await user.save();
+
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, 'secret');
+
+        req.session = {
+            jwt: userJwt
+        }
 
         res.status(201).send(user);
     });
