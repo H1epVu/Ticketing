646a1188384b64878bf6b07e6a1a14a753f1b811
H1ep Vu <hiepvu.tech02@gmail.com>
2025-03-07 18:37:37 +0700

    some test scenario

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,68 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it('failed login with email that does not exist', async () => {
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('failed login with incorrect password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password1'
+        })
+        .expect(400);
+});
+
+it('successful login', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    const response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    const response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
