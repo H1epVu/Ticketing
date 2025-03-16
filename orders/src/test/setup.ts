1c2bd8e627c6efe42240d0bdd87c0cb18f26c4a4
H1ep Vu <hiepvu.tech02@gmail.com>
2025-03-17 04:04:36 +0700

    refactor NATS wrapper and test setup; improve code formatting and remove unused utility functions

--- orders/src/test/setup.ts
+++ orders/src/test/setup.ts
@@ -1,34 +1,60 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 
+declare global {
+  var signin: () => string[];
+}
 jest.mock('../nats-wrapper');
 
-let mongo: MongoMemoryServer;
-
+let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = 'secret';
+  process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-    mongo = new MongoMemoryServer();
-    
-    const mongoUri = await mongo.getUri();
+  const mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
 
-    await mongoose.connect(mongoUri);
+  await mongoose.connect(mongoUri, {});
 });
 
 beforeEach(async () => {
-    jest.clearAllMocks();
-    const db = mongoose.connection.db;
-    if (!db) {
-        throw new Error('Database connection is not established');
-    }
-    const collections = await db.collections();
+  jest.clearAllMocks();
+  if (mongoose.connection.db) {
+    const collections = await mongoose.connection.db.collections();
 
     for (let collection of collections) {
-        await collection.deleteMany({});
+      await collection.deleteMany({});
     }
+  }
 });
 
 afterAll(async () => {
+  if (mongo) {
     await mongo.stop();
-    await mongoose.connection.close();
+  }
+  await mongoose.connection.close();
 });
+
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
