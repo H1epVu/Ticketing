3394134ba2337f4b3f8e89209a1b68a66b76334c
H1ep Vu <hiepvu.tech02@gmail.com>
2025-03-17 04:04:56 +0700

    refactor order routes to improve parameter naming and code consistency; enhance readability and maintainability

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import {
-  NotAuthorizedError,
-  NotFoundError,
   requireAuth,
+  NotFoundError,
+  NotAuthorizedError
 } from '@henryvux02/common';
-
 import { Order, OrderStatus } from '../models/order';
 import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
 import { natsWrapper } from '../nats-wrapper';
@@ -12,34 +11,33 @@ import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
 router.delete(
-  '/api/orders/:id',
+  '/api/orders/:orderId',
   requireAuth,
   async (req: Request, res: Response) => {
-    const { id } = req.params;
+    const { orderId } = req.params;
 
-    const order = await Order.findById(id).populate('ticket');
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
     }
-
     if (order.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
-
     order.status = OrderStatus.Cancelled;
     await order.save();
 
+    // publishing an event saying this was cancelled!
     new OrderCancelledPublisher(natsWrapper.client).publish({
       id: order.id,
       version: order.version,
       ticket: {
-        id: order.ticket.id,
-      },
+        id: order.ticket.id
+      }
     });
 
     res.status(204).send(order);
-  },
+  }
 );
 
 export { router as deleteOrderRouter };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,63 +1,76 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@henryvux02/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError
+} from '@henryvux02/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
-
 import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
-const EXPIRE_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
-router.post('/api/orders', requireAuth, [
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
     body('ticketId')
-        .not()
-        .isEmpty()
-        .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-        .withMessage('TicketId must be provided')
-], validateRequest,
-    async (req: Request, res: Response) => {
-        const { ticketId } = req.body;
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided')
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
 
-        const ticket = await Ticket.findById(ticketId);
-        if (!ticket) {
-            throw new NotFoundError();
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
 
-        }
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
 
-        const isReserved = await ticket.isReserved();
-        if (isReserved) {
-            throw new BadRequestError('Ticket is already reserved');
-        }
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
-        const expiration = new Date();
-        expiration.setSeconds(expiration.getSeconds() + EXPIRE_WINDOW_SECONDS);
-
-        const order = Order.build({
-            userId: req.currentUser!.id,
-            status: OrderStatus.Created,
-            expiresAt: new Date(),
-            ticket
-        });
-
-        await order.save();
-
-        new OrderCreatedPublisher(natsWrapper.client).publish({
-            id: order.id,
-            status: order.status,
-            userId: order.userId,
-            expiresAt: order.expiresAt.toISOString(),
-            version: order.version,
-            ticket: {
-                id: ticket.id,
-                price: ticket.price,
-            },
-        });
-
-        res.status(201).send(order);
+    // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket
     });
+    await order.save();
+
+    // Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      version: order.version,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price
+      }
+    });
+
+    res.status(201).send(order);
+  }
+);
 
 export { router as newOrderRouter };
